832d45dd044e6a557d86689ef535d1a96d622146
jpkamble <98741006+jpkamble@users.noreply.github.com>
2022-01-31 10:07:46 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<html> 
+	<head> 
+	<title>PHP</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
+	</head>
+
+<body> 
+	<h1>PHP 2022  Test</h1>
+		<p><b> CITIBANK JEETH007 </b></p>
+		<?php echo "The Current Date and yes las  Time is: <br />";
+		echo date("g:i A l, F j Y.");?> </p>
+	<h2>PHP Information VERSION 2 </h2> 
+		<p> <?php phpinfo(); ?> </p> 
+	</body> 
+</html>
